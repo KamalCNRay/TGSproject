19f3bf995ba4177635a51ecf331b3c8270782041
kamal.cnray@gmail.com <kamal.cnray@gmail.com>
2020-04-22 11:20:47 +0530

    adding some more files

---
+++ WorkSpace/Object Repository/HomePage/SessionVerficationFrame.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Frame to verify overriding existing session</description>
+   <name>SessionVerficationFrame</name>
+   <tag></tag>
+   <elementGuidId>8ca5cd0e-9c83-410a-baa6-3ab54f6b3499</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class ='os-internal-ui-widget-overlay']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
